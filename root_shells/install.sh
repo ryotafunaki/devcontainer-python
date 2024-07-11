b8071f3be91aed276318f17cae73308c78ae10a9
Ryota Funaki <contact@ngv.jp>
2024-07-11 21:42:24 +0900

    Improve for Docker Hub Automated Build (#10)

---
+++ root_shells/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copyright (c) 2024 RFull Development
+# This source code is managed under the MIT license. See LICENSE in the project root.
+
+# Execute all scripts in the shells.d directory
+TARGET_FILES=$(find shells.d -type f -name "*.sh" | sort)
+for file in $TARGET_FILES; do
+    bash "$file"
+done
+
+# Clean up
+apt clean
+rm -rf /var/lib/apt/lists/*
