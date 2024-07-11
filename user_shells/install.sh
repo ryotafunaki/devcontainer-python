f620f689832ef5eb1336b39021e191249b5ed5aa
Ryota Funaki <contact@ngv.jp>
2024-07-12 07:19:53 +0900

    Update install.sh (#12)

--- user_shells/install.sh
+++ user_shells/install.sh
@@ -7,3 +7,12 @@ TARGET_FILES=$(find shells.d -type f -name "*.sh" | sort)
 for file in $TARGET_FILES; do
     bash "$file"
 done
+
+# Add path to the .bashrc file
+cat << EOT >> ~/.bashrc
+
+# Add .NET tools to the PATH
+if [ -d "$HOME/.dotnet/tools" ]; then
+    export PATH="$PATH:$HOME/.dotnet/tools"
+fi
+EOT
